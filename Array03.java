346d68871a81fc0d7db7c51ae67e7a3be0c1d31e
ErasmoDeveloper <erasmo.network@gmail.com>
2022-08-10 09:49:19 -0300

    Create Array03.java

---
+++ Array03.java
@@ -0,0 +1,19 @@
+package br.com.impacta.arrays;
+
+public class Array03 {
+    public static void main(String[] args) {
+
+        int[] numeros = {17,13,18,25,39,17};
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+        System.out.println();
+
+        //Array vai referenciar outro objeto
+        numeros = new int[] {2, 5, 7, 1, 0};
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
+}

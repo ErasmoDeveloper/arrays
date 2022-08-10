c54ce38e9861e1ed81086fa5dac7cc95bd4e1a70
ErasmoDeveloper <erasmo.network@gmail.com>
2022-08-10 09:49:29 -0300

    Create Array06.java

---
+++ Array06.java
@@ -0,0 +1,15 @@
+package br.com.impacta.arrays;
+
+public class Array06 {
+    public static void main(String[] args) {
+        /*Escreva um programa para somar os valores do array*/
+        int [] n = {1,2,3,4,5,6,7,8,9,10};
+
+        int soma = 0;
+
+        for (int numeroAtual : n) {
+            soma = soma + numeroAtual;
+        }
+        System.out.println("A soma de todos os valores: " + soma);
+    }
+}

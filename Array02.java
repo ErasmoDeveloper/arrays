ab60487354c54c0b88d4e2861f5679b8ae02f1cd
ErasmoDeveloper <erasmo.network@gmail.com>
2022-08-10 09:49:14 -0300

    Create Array02.java
    Upload

---
+++ Array02.java
@@ -0,0 +1,12 @@
+package br.com.impacta.arrays;
+
+public class Array02 {
+    public static void main(String[] args) {
+
+        int [] numeros = {17,13,18,25,39,17};
+
+        for (int elemento : numeros) {
+            System.out.println(elemento);
+        }
+    }
+}
